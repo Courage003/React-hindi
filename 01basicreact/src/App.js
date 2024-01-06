877bcf84e5aae005292af477f13101fe94e1e8b8
Dhruv Rohatgi <rastogidhruv28@gmail.com>
2024-01-07 00:18:26 +0530

    Create React Projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>Hello world! | Learnt about creating proect from perspectives of react and vite</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+function App() {
+  
+
+  return (
+    <h1>Chai aur react with Vite | Dhruv Rphatgi</h1>
+      
+  )
+}
+
+export default App
