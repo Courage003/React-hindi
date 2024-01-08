317a2ebe4084f93aef90601337e01a00aa5c1207
Dhruv Rohatgi <rastogidhruv28@gmail.com>
2024-01-08 18:15:03 +0530

    Counter Project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+  // (let counter, method to control the variable)
+
+  //let counter = 5
+
+  const addValue = ()=>{
+    console.log("Clicked", counter)
+    //counter = counter + 1
+    if(counter<10){
+      setCounter(counter + 1)
+    }
+    
+  }
+
+  const removeValue = ()=>{
+    if(counter>0){
+      setCounter(counter - 1)
+    }
+    
+  }
+
+  //Data updation in UI through hooks (Reactive Library)
+ 
+
+  return (
+    <>
+      <h1>React Series</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button
+      onClick={addValue}>Add Value {counter}</button>
+      <br />
+      <button
+      onClick={removeValue}>Remove Value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
