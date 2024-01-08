caeda5db9bee28f445fbfe471fc97ec50d51be69
Dhruv Rohatgi <rastogidhruv28@gmail.com>
2024-01-08 17:36:41 +0530

    Creating own react Library

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Tut from "./tut"
 function App() {
+  const username = "Dhruv"
   
 
   return (
     <> 
         <Tut />
-        <h1> React is been learnt</h1>
+        <h1> React is been learnt {username}</h1>
         <p>test para</p>
     </>
 
@@ -18,4 +19,6 @@ export default App
 /*JSX can return only single element at once
 to resolve this issue div wrapper is used and since it has to be used quite often it's replaced by
 <> </>  (fragment)*/
+
+/*{username} such is termed as evaluated expression (JS)*/
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+//import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+/*function MyApp(){
+  return(
+    <>
+    <h1>Custom App</h1>
+    </>
+  )
+}*/
+
+/*const ReactElement = {
+  type : 'a',
+  props: {
+      href: 'https://google.com',
+      target: '_blank'
+
+  },
+  children: 'Click me to visit google'
+}*/
+
+/*const AnotherElement = (
+  <a href="https://google.com" target="_blank">Visit Google</a>
+)*/
+
+//syntax proper as per render
+
+//proper react syntax as per React render system (bable)
+
+const anotherUser = "Dhruv Rohatgi"
+const ReactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google',
+  anotherUser
 )
+
+//significance of evaluated expression , we cannot write full js code inside an object
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    //<ReactElement/>  //synatx was not proper as per custom render
+    //Object will not get exceuted
+    //AnotherElement
+    ReactElement
+    //<App/>
+  
+)
+
+//React uses bundler to improvise syntax etc. (JSX)
